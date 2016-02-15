9b30abbbfcccc5e4e00f572c4968566b90a50832
James Atherton <james@bitovi.com>
2016-02-15 10:32:58 -0600

    Closes #16. First user created is an admin by default. Updated gitignore for the DS_Store and log files.

--- services/users.js
+++ services/users.js
@@ -19,26 +19,26 @@ passport.use('signup', new LocalStrategy({
 				console.log("POST USERS: use already exists" + username, user);
 				done(null, false);
 			} else {
-				console.log("POST USERS: creating a new user");
-				var newUser = new User({
-					email: username,
-					password: createHash(password),
-					isAdmin: username === "justin@bitovi.com" || username === "justinbmeyer@gmail.com"
-				});
-				// set the user's local credentials
-	
-				// save the user
-				newUser.save().then(function(err) {
-					console.log('User Registration succesful');
-					return done(null, newUser);
-				}, function(err) {
-					console.log('Error in Saving user: ' + err);
+				User.count().then( function ( numberOfUsers ) {
+					//bookshelf returns a string for the result of a count.
+					numberOfUsers = parseInt( numberOfUsers, 10 ) || 0;
+					console.log( "POST USERS: creating a new user. Current count of users:", numberOfUsers );
+					var newUser = new User({
+						email: username,
+						password: createHash(password),
+						isAdmin: !numberOfUsers
+					});
+					// set the user's local credentials
+		
+					// save the user
+					newUser.save().then(function(err) {
+						console.log('User Registration succesful');
+						return done(null, newUser);
+					}, function(err) {
+						console.log('Error in Saving user: ' + err);
+					});
 				});
 			}
-			
-			
-			
-
 		}, function() {
 			
 		});
@@ -48,7 +48,6 @@ passport.use('signup', new LocalStrategy({
 }));
 
 app.post('/services/users', passport.authenticate('signup'), function(req, res) {
-
 	res.send(_.omit(req.user.toJSON(), "password"));
 });
 
