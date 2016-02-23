471e619282b65b21cffb51508e6b3be922b81d52
Jan Jorgensen <jandjorgensen@gmail.com>
2016-02-23 14:52:00 -0700

    fix player tests by updating fixtures fixes #18

--- public/components/player/test.js
+++ public/components/player/test.js
@@ -1,6 +1,4 @@
+import './edit/edit_test';
+
 import './detail/detail_test';
-
-// TODO: make the player vs players fixtures play nice
-// import './edit/edit_test';
 import './list/player_list_test';
-
