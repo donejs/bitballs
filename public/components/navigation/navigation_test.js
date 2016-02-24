5048d37d02b72f5acb087322fe90e1ab46cc927b
Chris Gomez <akagomez@gmail.com>
2016-02-24 16:18:32 -0500

    Prevent Bootstrap from collapsing navigation menus at small screen resolutions

---
+++ public/components/navigation/navigation_test.js
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+import can from 'can';
+import 'can/view/stache/stache';
+import QUnit from 'steal-qunit';
+import F from 'funcunit';
+import testUtils from 'bitballs/test/utils';
+import './navigation';
+
+F.attach(QUnit);
+
+QUnit.module('bitballs-navigation', {
+    beforeEach: function () {
+        var frag = can.stache('<bitballs-navigation></bitballs-navigation>')();
+        testUtils.insertAndPopulateIframe('#qunit-fixture', frag);
+    },
+    afterEach: function () {
+        $('#qunit-fixture').empty();
+    }
+});
+
+QUnit.test('Layout preserved at smaller screen resolutions', function (assert) {
+    var evaluateAtWidth = function (resolution) {
+        // Set the width
+        F('#qunit-fixture iframe').then(function () {
+
+            // For some reason the query needs to be redone
+            $(this.selector).css('width', resolution);
+        });
+
+        // Confirm the styles
+        F('.session-menu')
+            .visible('Session menu is visible at ' + resolution)
+            .css('float', 'right',
+                'Session menu is floated right at ' + resolution);
+        F('.main-menu')
+            .visible('Main menu is visible at ' + resolution)
+            .css('float', 'left',
+                'Main menu is floated left at ' + resolution);
+    };
+
+    evaluateAtWidth('1170px');
+    evaluateAtWidth('750px');
+});
