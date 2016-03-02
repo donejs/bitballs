c1b3c7302884c7b483237aee17f6635904063e37
James Atherton <james@bitovi.com>
2016-03-02 13:59:57 -0600

    Permissions for adminOnly actions. #41

--- services/games.js
+++ services/games.js
@@ -3,6 +3,7 @@ var app = require("../app");
 var bookshelf = require("../bookshelf");
 var Stat = require("./stats");
 var Team = require("./teams");
+var adminOnly = require( "../adminOnly" );
 
 var Game = bookshelf.Model.extend({
 	tableName: 'games',
@@ -16,6 +17,7 @@ var Game = bookshelf.Model.extend({
 		return this.belongsTo(Team,"awayTeamId");
 	}
 });
+
 var Games = bookshelf.Collection.extend({
   model: Game
 });
@@ -31,23 +33,23 @@ app.get('/services/games/:id', function(req, res){
 		res.send(game.toJSON());
 	});
 });
-app.put('/services/games/:id', function(req, res){
+
+app.put('/services/games/:id', adminOnly( "Must be an admin to update games" ), function(req, res){
 	new Game({id: req.params.id}).save(req.body).then(function(game){
 		res.send(game.toJSON());
 	});
 });
 
-app['delete']('/services/games/:id', function(req, res){
+app['delete']('/services/games/:id', adminOnly( "Must be an admin to delete games" ), function(req, res){
 	new Game({id: req.params.id}).destroy().then(function(game){
 		res.send({});
 	});
 });
 
-app.post('/services/games', function(req, res) {
+app.post('/services/games', adminOnly( "Must be an admin to create games" ), function(req, res) {
 	new Game(req.body).save().then(function(game){
 		res.send({id: game.get('id')});
 	}, function(e){
 		res.status(500).send(e);
 	});
-
 });
