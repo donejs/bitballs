288762467aace36249def9edba7d250a6ac77f32
Chris Gomez <akagomez@gmail.com>
2016-03-07 11:25:24 -0500

    Remove app.json and db_client.js (#75)

--- db_client.js
+++
@@ -1,6 +0,0 @@
-var pg = require('pg'),
-	connectionString = process.env.DATABASE_URL || process.env.HEROKU_POSTGRESQL_TEAL_URL || 'postgres://localhost:5432/bitballs',
-	client = new pg.Client(connectionString);
-	client.connect();
-	
-module.exports = client;
