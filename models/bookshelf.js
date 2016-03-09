6bd587a60ab4b318360a618596ee271758415174
Chris Gomez <akagomez@gmail.com>
2016-03-09 11:40:28 -0500

    Use db-migrate config to configure bookshelf (#75)

--- models/bookshelf.js
+++ models/bookshelf.js
@@ -1,4 +1,11 @@
-var connectionString = process.env.DATABASE_URL || process.env.HEROKU_POSTGRESQL_TEAL_URL || 'postgres://localhost:5432/bitballs';
+var dbConfig = require('../database.json');
+var environmentKey = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
+var dbEnvironmentConfig = dbConfig[environmentKey];
+
+// Use the string itself or use the provided environment variable
+var connectionString = typeof dbEnvironmentConfig === 'string' ?
+    dbEnvironmentConfig :
+    process.env[dbEnvironmentConfig.ENV];
 
 var knex = require('knex')({
   client: 'pg',
