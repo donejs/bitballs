d1ae93ebb6937effb62c873011ec83b637c0962b
Justin Meyer <justinbmeyer@gmail.com>
2016-03-16 21:00:03 -0500

    jshint

--- public/components/user/list/list_test.js
+++ public/components/user/list/list_test.js
@@ -1,10 +1,8 @@
 import QUnit from 'steal-qunit';
-import { ViewModel } from './list';
 
 // ViewModel unit tests
 QUnit.module('bitballs/components/user/list');
 
 QUnit.test('Has message', function(){
-  var vm = new ViewModel();
   QUnit.ok(true, 'Has a test');
 });
