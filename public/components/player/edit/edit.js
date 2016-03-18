a81986f0e1b9e6731c0d4d817ea6490d52ac7e7c
Chris Gomez <akagomez@gmail.com>
2016-03-18 14:49:30 -0400

    Document, demo, and cleanup player-edit component (#185)

--- public/components/player/edit/edit.js
+++ public/components/player/edit/edit.js
@@ -1,3 +1,33 @@
+/**
+ * @module {Module} bitballs/components/player/edit <player-edit>
+ * @parent bitballs.components
+ *
+ * @group bitballs/components/player/edit.properties 0 properties
+ *
+ * @description Provides an interface for editing the values of a
+ * [bitballs/models/player] model.
+ *
+ * @signature `<player-edit {is-admin} />`
+ *   Creates a form with inputs for each property in a [bitballs/models/player] model.
+ *
+ *   @param {Boolean} is-admin Configures whether or not admin specific
+ *   features are enabled.
+ *
+ *
+ * @body
+ *
+ * To create a `<player-edit>` element pass a boolean like [bitballs/app.prototype.isAdmin]:
+ *
+ * ```
+ * <player-edit
+ *     {is-admin}="app.isAdmin" />
+ * ```
+ *
+ * ## Example
+ *
+ * @demo public/components/player/edit/edit.html
+ *
+ **/
 var Component = require("can/component/component");
 var Player = require("bitballs/models/player");
 var CanMap = require("can/map/");
@@ -8,14 +38,47 @@ require('can/map/backup/');
 require("can/construct/super/");
 
 
-exports.ViewModel = CanMap.extend({
+exports.ViewModel = CanMap.extend(
+/** @prototype **/
+{
 	define: {
+		/**
+		* @property {Boolean} bitballs/components/player/edit.isAdmin isAdmin
+		* @parent bitballs/components/player/edit.properties
+		*
+		* Configures whether or not admin specific features are enabled.
+		**/
+		isAdmin: {
+			type: 'boolean',
+			value: false
+		},
+		/**
+		* @property {bitballs/models/player} bitballs/components/player/edit.player player
+		* @parent bitballs/components/player/edit.properties
+		*
+		* The model that will be bound to the form.
+		**/
 		player: {
 			Value: Player,
 			Type: Player
 		}
 	},
-	savePlayer: function(){
+	/**
+	 * @function savePlayer
+	 *
+	 * Creates/updates the player on the server and when successful sets [bitballs/components/player/edit.player]
+	 * to a new [bitballs/models/player] model. Fires a "saved" event.
+	 *
+	 * @param {Event} [ev] A DOM Level 2 event that [`preventDefault`](https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault)
+	 * will be called on.
+	 *
+	 * @return {Promise<Player>}
+	 */
+	savePlayer: function(ev){
+		if (ev) {
+			ev.preventDefault();
+		}
+
 		var self = this;
 		var player = this.attr("player"),
 			promise;
@@ -37,6 +100,14 @@ exports.ViewModel = CanMap.extend({
 
 		return promise;
 	},
+	/**
+	 * @function cancel
+	 *
+	 * Restores the [bitballs/models/player] model to its state prior to editing.
+	 * Fires a "canceled" event.
+	 *
+	 * @return {undefined}
+	 */
 	cancel: function() {
 		this.attr('player').restore();
 		this.dispatch("canceled");
@@ -46,10 +117,5 @@ exports.ViewModel = CanMap.extend({
 exports.Component = Component.extend({
 	tag: "player-edit",
 	template: require("./edit.stache!"),
-	viewModel: exports.ViewModel.extend({
-		savePlayer: function(ev) {
-			ev.preventDefault();
-			this._super.apply(this,arguments);
-		}
-	})
+	viewModel: exports.ViewModel
 });
