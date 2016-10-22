41190ade87c3fab5015666c67efc8f00f3f94f6b
Frank Lemanschik <frank@github.dspeed.eu>
2016-10-22 14:25:13 +0100

    Update app.js
    We need to be lazzy because bitballs don't posts valid json.

--- services/app.js
+++ services/app.js
@@ -5,6 +5,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json 
-app.use(bodyParser.json());
+// as bitballs don't posts valid json it url encodes it or some what to
+// "{ "":"" }" whats not { "":"" } we need to use strict: false
+app.use(bodyParser.json({ strict: false}));
 
 module.exports = app;
