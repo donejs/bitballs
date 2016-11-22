edc5ee984b36619b55595f2af51216e3112e6c17
Obaid Ahmed <obaid@bitovi.com>
2016-11-22 17:06:28 -0500

    Disabling test case to isolate firefox timeout issue

--- public/components/tournament/details/details_test.js
+++ public/components/tournament/details/details_test.js
@@ -1,80 +1,80 @@
-// import QUnit from 'steal-qunit';
-// import details from './details';
-// import defineTournamentFixtures from 'bitballs/models/fixtures/tournaments';
-// import 'bitballs/models/fixtures/players';
-// import defineGameFixtures  from 'bitballs/models/fixtures/games';
-// import fixture from "can-fixture";
-// import Game from 'bitballs/models/game';
+import QUnit from 'steal-qunit';
+import details from './details';
+import defineTournamentFixtures from 'bitballs/models/fixtures/tournaments';
+import 'bitballs/models/fixtures/players';
+import defineGameFixtures  from 'bitballs/models/fixtures/games';
+import fixture from "can-fixture";
+import Game from 'bitballs/models/game';
 
-// var ViewModel = details.ViewModel;
-// var vm;
+var ViewModel = details.ViewModel;
+var vm;
 
-// QUnit.module('components/tournament/details/', {
-//     beforeEach: function (assert) {
-//         let done = assert.async();
-//         localStorage.clear();
-//         defineTournamentFixtures();
-//         defineGameFixtures();
+QUnit.module('components/tournament/details/', {
+    beforeEach: function (assert) {
+        let done = assert.async();
+        localStorage.clear();
+        defineTournamentFixtures();
+        defineGameFixtures();
 
-//         vm = new ViewModel({
-//                 tournamentId: 2
-//             });
+        vm = new ViewModel({
+                tournamentId: 2
+            });
 
-//         done();        
-//     }
-// });
+        done();        
+    }
+});
 
-// QUnit.test('should load a tournament', (assert) => {
-//     let done = assert.async();
-//     vm.on('tournament', function (ev, newVal) {
-//         assert.equal(newVal.name, 'EBaller Virus', 'with the correct name' );
-//         done();
-//     });
-// });
+QUnit.test('should load a tournament', (assert) => {
+    let done = assert.async();
+    vm.on('tournament', function (ev, newVal) {
+        assert.equal(newVal.name, 'EBaller Virus', 'with the correct name' );
+        done();
+    });
+});
 
-// QUnit.test('The selected round defaults to the first available round', function () {
-//     var vm = new ViewModel();
+QUnit.test('The selected round defaults to the first available round', function () {
+    var vm = new ViewModel();
     
-//     var gamesResponse = { data: [] };
+    var gamesResponse = { data: [] };
 
-//     Game.courtNames.forEach(function (courtName) {
-//         gamesResponse.data.push({
-//             round: Game.roundNames[0],
-//             court: courtName
-//         });
-//     });
+    Game.courtNames.forEach(function (courtName) {
+        gamesResponse.data.push({
+            round: Game.roundNames[0],
+            court: courtName
+        });
+    });
 
-//     fixture('/services/games', function() {
-//         return gamesResponse;
-//     });
+    fixture('/services/games', function() {
+        return gamesResponse;
+    });
 
-//     vm.on("selectedRound", function(){});
-//     QUnit.stop();
-//     vm.gamesPromise.then(function (games) {
-//         QUnit.start();
-//         QUnit.equal(vm.selectedRound, Game.roundNames[1],
-//             'The second round is selected');
-//     });
-// });
+    vm.on("selectedRound", function(){});
+    QUnit.stop();
+    vm.gamesPromise.then(function (games) {
+        QUnit.start();
+        QUnit.equal(vm.selectedRound, Game.roundNames[1],
+            'The second round is selected');
+    });
+});
 
-// QUnit.test('The selected court defaults to the first available court', function () {
-//     var vm = new ViewModel();
+QUnit.test('The selected court defaults to the first available court', function () {
+    var vm = new ViewModel();
     
-//     var gamesResponse = { data: [{
-//         round: Game.roundNames[0],
-//         court: Game.courtNames[0]
-//     }] };
+    var gamesResponse = { data: [{
+        round: Game.roundNames[0],
+        court: Game.courtNames[0]
+    }] };
 
-//     fixture('/services/games', function() {
-//         return gamesResponse;
-//     });
+    fixture('/services/games', function() {
+        return gamesResponse;
+    });
 
-//     QUnit.stop();
-//     vm.on("selectedCourt", function(){});
-//     vm.gamesPromise.then(function (games) {
-//         QUnit.start();
-//         vm.on('selectedCourt', function(){});
-//         QUnit.equal(vm.selectedCourt, Game.courtNames[1],
-//             'The second court is selected');
-//     });
-// });
+    QUnit.stop();
+    vm.on("selectedCourt", function(){});
+    vm.gamesPromise.then(function (games) {
+        QUnit.start();
+        vm.on('selectedCourt', function(){});
+        QUnit.equal(vm.selectedCourt, Game.courtNames[1],
+            'The second court is selected');
+    });
+});
