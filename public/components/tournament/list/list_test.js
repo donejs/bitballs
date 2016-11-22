b84abc7fe76abeb451e5fb030f39430bcf9b54f4
Obaid Ahmed <obaid@bitovi.com>
2016-11-22 16:55:23 -0500

    Disabling test case to isolate firefox timeout issue

--- public/components/tournament/list/list_test.js
+++ public/components/tournament/list/list_test.js
@@ -1,80 +1,80 @@
-// import $ from 'jquery';
-// import stache from 'can-stache';
-// import fixture from 'can-fixture';
-// import QUnit from 'steal-qunit';
-// import F from 'funcunit';
-// import tournamentList from './list';
-// import defineFixtures from 'bitballs/models/fixtures/tournaments';
+import $ from 'jquery';
+import stache from 'can-stache';
+import fixture from 'can-fixture';
+import QUnit from 'steal-qunit';
+import F from 'funcunit';
+import tournamentList from './list';
+import defineFixtures from 'bitballs/models/fixtures/tournaments';
 
-// F.attach(QUnit);
+F.attach(QUnit);
 
-// var ViewModel = tournamentList.ViewModel;
+var ViewModel = tournamentList.ViewModel;
 
-// QUnit.module('components/tournament/list/', {
-// 	beforeEach: function () {
-// 		localStorage.clear();
-// 		fixture.delay = 1;
-// 		defineFixtures();
-// 	}
-// });
+QUnit.module('components/tournament/list/', {
+	beforeEach: function () {
+		localStorage.clear();
+		fixture.delay = 1;
+		defineFixtures();
+	}
+});
 
-// QUnit.test('creating tournament fails without a name', function(assert){
-// 	var done = assert.async();
+QUnit.test('creating tournament fails without a name', function(assert){
+	var done = assert.async();
 
-// 	assert.expect(2);
+	assert.expect(2);
 
-// 	var vm = new ViewModel();
+	var vm = new ViewModel();
 
-// 	vm.createTournament();
-// 	vm.savePromise.then(done, function(resp, type){
-// 		assert.equal(resp.statusText, 'error', 'fail creation without date');
-// 		assert.equal(resp.status, '400', 'rejected');
-// 		done();
-// 	});
-// });
+	vm.createTournament();
+	vm.savePromise.then(done, function(resp, type){
+		assert.equal(resp.statusText, 'error', 'fail creation without date');
+		assert.equal(resp.status, '400', 'rejected');
+		done();
+	});
+});
 
-// QUnit.test('Create button is disabled while posting data', function (assert) {
-// 	var done = assert.async();
-// 	var expectingRequest = true;
-// 	var vm = new ViewModel({
-//         app: {
-//             isAdmin: true
-//         },
-//         tournament: {
-//             name: 'Ballderdash',
-//             date: '01/21/1987'
-//         }
-//     });
+QUnit.test('Create button is disabled while posting data', function (assert) {
+	var done = assert.async();
+	var expectingRequest = true;
+	var vm = new ViewModel({
+        app: {
+            isAdmin: true
+        },
+        tournament: {
+            name: 'Ballderdash',
+            date: '01/21/1987'
+        }
+    });
 
 	
-// 	var frag = stache('<tournament-list {is-admin}="app.isAdmin" {tournament}="tournament" />')(vm);
-// 	var resolveRequest;
+	var frag = stache('<tournament-list {is-admin}="app.isAdmin" {tournament}="tournament" />')(vm);
+	var resolveRequest;
 	
-//     fixture('POST /services/tournaments', function (req, res) {
-//         QUnit.ok(expectingRequest, 'Request was made');
+    fixture('POST /services/tournaments', function (req, res) {
+        QUnit.ok(expectingRequest, 'Request was made');
 
-//         // Determine when the request resolves, later
-//         resolveRequest = res;
+        // Determine when the request resolves, later
+        resolveRequest = res;
 
-//         // The request should only be made once
-//         expectingRequest = false;
-//     });
+        // The request should only be made once
+        expectingRequest = false;
+    });
 
-//     $('#qunit-fixture').html(frag);
+    $('#qunit-fixture').html(frag);
 
-//     // Click the button multiple times and ensure it's disabled
-//     // during requests
-//     F('tournament-list .create-btn')
-//         .visible('Create button is visible')
-//         .attr('disabled', undefined, 'Create button is enabled')
-// 				.click();
-// 	F('tournament-list .create-btn')
-// 			.attr('disabled', 'disabled', 'Create button is disabled')
-// 			.then(function() {
-// 				resolveRequest({});
-// 			})
-// 			.attr('disabled', undefined,
-// 		'Create button is enabled after the request is resolved');
+    // Click the button multiple times and ensure it's disabled
+    // during requests
+    F('tournament-list .create-btn')
+        .visible('Create button is visible')
+        .attr('disabled', undefined, 'Create button is enabled')
+				.click();
+	F('tournament-list .create-btn')
+			.attr('disabled', 'disabled', 'Create button is disabled')
+			.then(function() {
+				resolveRequest({});
+			})
+			.attr('disabled', undefined,
+		'Create button is enabled after the request is resolved');
 
-// 	done();
-// });
+	done();
+});
