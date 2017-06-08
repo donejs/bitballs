00ee5a8171ded2379f1005d88638db7d9c911e47
Justin Meyer <justinbmeyer@gmail.com>
2017-06-08 11:20:32 -0500

    removing numberToString and getting tests to pass

--- public/components/player/details/details-test.js
+++ public/components/player/details/details-test.js
@@ -1,10 +1,4 @@
 import QUnit from 'steal-qunit';
-import { ViewModel } from './details';
 
 // ViewModel unit tests
 QUnit.module('bitballs/components/player/details');
-
-QUnit.test('Has message', function(){
-  var vm = new ViewModel();
-  QUnit.equal(vm.message, 'This is the player-details component');
-});
